c06caf317bf5cf68627584f8413d1e8d3b519882
Sam McCune <sjm4593@g.rit.edu>
2022-02-19 12:35:14 -0500

    Initial Commit w/ Task 1 additions (not including testing)

---
+++ code-challenge/Models/ReportingStructure.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challenge.Models
+{
+   // Task 1
+    public class ReportingStructure
+    {
+        public Employee ReportEmployee { get; set; }
+
+        public int NumberOfReports { get; set; }
+    }
+}
---
+++ code-challenge/Services/EmployeeService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Models;
+using Microsoft.Extensions.Logging;
+using challenge.Repositories;
+
+namespace challenge.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ILogger<EmployeeService> _logger;
+
+        public EmployeeService(ILogger<EmployeeService> logger, IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _logger = logger;
+        }
+
+        public Employee Create(Employee employee)
+        {
+            if(employee != null)
+            {
+                _employeeRepository.Add(employee);
+                _employeeRepository.SaveAsync().Wait();
+            }
+
+            return employee;
+        }
+
+        public Employee GetById(string id)
+        {
+            if(!String.IsNullOrEmpty(id))
+            {
+                return _employeeRepository.GetById(id);
+            }
+
+            return null;
+        }
+
+        public Employee Replace(Employee originalEmployee, Employee newEmployee)
+        {
+            if(originalEmployee != null)
+            {
+                _employeeRepository.Remove(originalEmployee);
+                if (newEmployee != null)
+                {
+                    // ensure the original has been removed, otherwise EF will complain another entity w/ same id already exists
+                    _employeeRepository.SaveAsync().Wait();
+
+                    _employeeRepository.Add(newEmployee);
+                    // overwrite the new id with previous employee id
+                    newEmployee.EmployeeId = originalEmployee.EmployeeId;
+                }
+                _employeeRepository.SaveAsync().Wait();
+            }
+
+            return newEmployee;
+        }
+        
+        // Task 1 Reporting Structure
+        // Added to get the reporting structure given an employee
+        public ReportingStructure GetReportingStructureWithId(string id)
+        {
+            // Get the employee and create reporting structure
+            Employee employee = GetById(id);
+            ReportingStructure reportingStructure = new ReportingStructure();
+
+            // Recursively generate reporting structure
+            var reports = employee.DirectReports;
+            foreach (var t in reports)
+            {
+                ReportingStructure temp = GetReportingStructureWithId(t.EmployeeId);
+                reportingStructure.NumberOfReports += 1 + temp.NumberOfReports;
+            }
+            
+            // Return the value
+            return reportingStructure;
+        }
+    }
+}
